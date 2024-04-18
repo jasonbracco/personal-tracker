193e7f376dcd6295c62ffba0edd2ded4284a3d54
Jason Bracco <jasonbracco@jasons-laptop.mynetworksettings.com>
2024-04-18 15:01:10 -0400

    only activities performed in the selected month will display on the calendar

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,19 @@ function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
 
+  console.log(activities)
+
   const addActivity = (activityName: string, date: string, startTime: string, endTime: string, location?: string, notes?: string, ) => {
     console.log("Made it to App");
     setActivities([...activities, {activityName, location, date, startTime, endTime, notes}])
   }
+
   return (
     <div className="App">
       <h1>Calendar App To Remember What You Did!</h1>
       <ActivityInputForm onAddActivity={addActivity} />
       <ActivityList activities={activities} />
-      <Calendar />
+      <Calendar activities={activities}/>
     </div>
   );
 }
